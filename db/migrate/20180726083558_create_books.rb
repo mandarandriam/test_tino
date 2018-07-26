6b78fbdd8bf6c2f62abeba4d65ca57cd24b05047
mandarandriam <mandarandriam@gmail.com>
2018-07-26 11:40:04 +0300

    tino_test

---
+++ db/migrate/20180726083558_create_books.rb
@@ -0,0 +1,10 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+	def change
+		create_table :books do |t|
+			t.belongs_to :author, index: true
+			t.string :title
+			t.string :these
+			t.timestamps
+		end
+	end
+end
